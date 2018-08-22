0b4af428e74d98f177d005599ee9c82f60e6d09b
PawilIOS <wileypatrick36@gmail.com>
2018-08-22 18:17:30 +0200

    Add Classes

---
+++ The Return Of The Wizard/Game/Game.swift
@@ -0,0 +1,9 @@
+//
+//  Game.swift
+//  The Return Of The Wizard
+//
+//  Created by Patrick Wiley on 22/08/2018.
+//  Copyright © 2018 Patrick Wiley. All rights reserved.
+//
+
+import Foundation
---
+++ The Return Of The Wizard/Heroes/Characters/Warrior.swift
@@ -0,0 +1,9 @@
+//
+//  Warrior.swift
+//  The Return Of The Wizard
+//
+//  Created by Patrick Wiley on 22/08/2018.
+//  Copyright © 2018 Patrick Wiley. All rights reserved.
+//
+
+import Foundation
