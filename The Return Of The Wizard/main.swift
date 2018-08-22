e5776f242c5426832b34988cb03d92fe12ed3bcf
PawilIOS <wileypatrick36@gmail.com>
2018-08-22 17:57:13 +0200

    Initial Commit

---
+++ The Return Of The Wizard/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  The Return Of The Wizard
+//
+//  Created by Patrick Wiley on 22/08/2018.
+//  Copyright © 2018 Patrick Wiley. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
