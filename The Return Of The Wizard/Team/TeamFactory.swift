d7179caacf546e02bfa13919a3ffbd6aa1882225
PawilIOS <wileypatrick36@gmail.com>
2018-09-05 00:36:00 +0200

    update TeamFactory_ adding initialization heroesFactory instance

--- The Return Of The Wizard/Team/TeamFactory.swift
+++ The Return Of The Wizard/Team/TeamFactory.swift
@@ -13,7 +13,8 @@ class TeamFactory {
     
     
     let teamFactory = Team()
-    
+    let heroesFactory = HeroesFactory()
+    let team = Team()
     
     func getStarterTeam() -> String{ // create the teamName
         
@@ -27,29 +28,40 @@ class TeamFactory {
     }
     
     
-    func composeTeam(teamName : String, heroesInt: String) -> Int{ // method createTeam
+    func composeTeam(teamName : String, heroesInt: String){ // method createTeam
         
-        let team = Team()
+        
         let choicePlayer = heroesInt
-        print(choicePlayer)
-        teamFactory.arrayTeam.append(HeroesFactory().getHeroe(heroesInt: choicePlayer)!)
-        //var arrayTeam = team.arrayTeam
+        let name = teamName
+        let heroe = heroesFactory.getHeroe(heroesInt: choicePlayer)!
+        heroe.teamName = name
+        teamFactory.arrayTeam.append(heroe)
         print("We have: \(teamFactory.arrayTeam.count) Heroes in the array of teams")
-        return teamFactory.arrayTeam.count
+        print(teamFactory.arrayTeam)
+        //let arrayTeamFactory = teamFactory.arrayTeam
         
     }
-//    func statusTeam(team: Team) {
-//        
-//        let index = team.arrayTeam.count
-//        print("that is the status of the team")
-//        print("I am in the StatusTeam method")
-//       
+    func statusTeam() {
+        
+        
+        print("that is the content of teamFactory.arrayTeam \(teamFactory.arrayTeam)")
+        
+        for element in 0..<teamFactory.arrayTeam.count {
+//            print("LifeStregth : \(teamFactory.arrayTeam[element].lifeStrength)")
+//            print("ShotStrength : \(teamFactory.arrayTeam[element].shotStrength)")
+//            print("ArmorStrength : \(teamFactory.arrayTeam[element].armorStrength)")
+//            print("Name : \(teamFactory.arrayTeam[element].name)")
+//            print("TeamName : \(teamFactory.arrayTeam[element].teamName)")
+            print("We have in Team  \(teamFactory.arrayTeam[element].teamName) :  A  \(teamFactory.arrayTeam[element].name) : with lifeStrength: \(teamFactory.arrayTeam[element].lifeStrength) and a shotStrength : \(teamFactory.arrayTeam[element].shotStrength) protected by \(teamFactory.arrayTeam[element].armorStrength) of armorStrength")
+            
+        }
+       
 //        print("We have: \(index) Heroes in the array of teams")
 //        print(index)    //should print the arrayTeam content
-//        
-//        
-//        
-//    }
+        
+        
+        
+    }
     func healing() {}
     func fight() {}
     
