c50352b4b2a6555a7eca4d3ac88405dd6a970489
PawilIOS <wileypatrick36@gmail.com>
2018-09-05 15:13:53 +0200

    adding chooseYourWeapon/ healing/ fight methods modification for composeTeam

--- The Return Of The Wizard/Team/TeamFactory.swift
+++ The Return Of The Wizard/Team/TeamFactory.swift
@@ -28,7 +28,7 @@ class TeamFactory {
     }
     
     
-    func composeTeam(teamName : String, heroesInt: String){ // method createTeam
+    func composeTeam(teamName : String, heroesInt: String)-> (String, String) { // method createTeam
         
         
         let choicePlayer = heroesInt
@@ -38,7 +38,68 @@ class TeamFactory {
         teamFactory.arrayTeam.append(heroe)
         print("We have: \(teamFactory.arrayTeam.count) Heroes in the array of teams")
         print(teamFactory.arrayTeam)
-        //let arrayTeamFactory = teamFactory.arrayTeam
+        return (name, choicePlayer)
+        
+    }
+    
+    func chooseYourWeapon(teamName : String, heroesInt: String, weaponChoice: String) { // method for choosing the Weapon // First not choosable
+        
+        let choicePlayer = heroesInt
+        let name = teamName
+        
+        let choiceWeaponPlayer = weaponChoice
+        switch choiceWeaponPlayer {
+        case "1" :
+            let equipment = Equipment.Axe
+            let equipmentEffect = equipment.rawValue // Printing the value of Axe
+            print(equipmentEffect)
+            
+            let heroe = heroesFactory.getHeroe(heroesInt: choicePlayer)!
+            heroe.teamName = name
+            heroe.equipment = equipment
+            
+           
+        case "2" :
+            
+            let equipment = Equipment.Hammer
+            let equipmentEffect = equipment.rawValue // Printing the value of Hammer
+            print(equipmentEffect)
+            
+            let heroe = heroesFactory.getHeroe(heroesInt: choicePlayer)!
+            heroe.teamName = name
+            heroe.equipment = equipment
+            
+        case "3" :
+            
+            let equipment = Equipment.Sword
+            let equipmentEffect = equipment.rawValue // Printing the value of Sword
+            print(equipmentEffect)
+            
+            let heroe = heroesFactory.getHeroe(heroesInt: choicePlayer)!
+            heroe.teamName = name
+            heroe.equipment = equipment
+        
+        case "4" :
+            
+            let equipment = Equipment.Scepter
+            let equipmentEffect = equipment.rawValue // Printing the value of Scepter
+            print(equipmentEffect)
+            
+            let heroe = heroesFactory.getHeroe(heroesInt: choicePlayer)!
+            heroe.teamName = name
+            heroe.equipment = equipment
+            
+        case "5" :
+            
+            let heroe = heroesFactory.getHeroe(heroesInt: choicePlayer)!
+            heroe.teamName = name
+            
+        default : print("You must choose a Weapon")
+                
+        }
+        
+        
+        
         
     }
     func statusTeam() {
@@ -52,7 +113,7 @@ class TeamFactory {
 //            print("ArmorStrength : \(teamFactory.arrayTeam[element].armorStrength)")
 //            print("Name : \(teamFactory.arrayTeam[element].name)")
 //            print("TeamName : \(teamFactory.arrayTeam[element].teamName)")
-            print("We have in Team  \(teamFactory.arrayTeam[element].teamName) :  A  \(teamFactory.arrayTeam[element].name) : with lifeStrength: \(teamFactory.arrayTeam[element].lifeStrength) and a shotStrength : \(teamFactory.arrayTeam[element].shotStrength) protected by \(teamFactory.arrayTeam[element].armorStrength) of armorStrength")
+            print("We have in Team  \(teamFactory.arrayTeam[element].teamName) :  A  \(teamFactory.arrayTeam[element].name) : with lifeStrength: \(teamFactory.arrayTeam[element].lifeStrength) and a shotStrength : \(teamFactory.arrayTeam[element].shotStrength) protected by \(teamFactory.arrayTeam[element].armorStrength) of armorStrength, is having \(teamFactory.arrayTeam[element].equipment) as an equipment")
             
         }
        
@@ -62,8 +123,25 @@ class TeamFactory {
         
         
     }
-    func healing() {}
-    func fight() {}
+    func healing() {
+        
+        if teamFactory.arrayTeam.isEmpty { // testing if the teams are created
+            print("You must create two teams")
+            game.gameMenu()
+        }
+        else {
+        print("You would like to heal someone")
+        }
+    }
+    func fight() {
+        if teamFactory.arrayTeam.isEmpty { // testing if the teams are created
+            print("You must create two teams")
+            game.gameMenu()
+        }
+        else {
+        print("You would like to fight against someone")
+        }
+    }
     
 }
 
