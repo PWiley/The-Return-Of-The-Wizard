7eb7fcba902d09220eee1dfc694faba56ba8b906
PawilIOS <wileypatrick36@gmail.com>
2018-09-07 14:22:27 +0200

    change statusTeam method -> passing parameter statusChoice (first or second team) change healing method -> add check wizard is in the team change status bool unique add checkNumberPlayerFirstTeam method add checkNumberPlayerSecondTeam method

--- The Return Of The Wizard/Team/TeamFactory.swift
+++ The Return Of The Wizard/Team/TeamFactory.swift
@@ -11,24 +11,20 @@ import Foundation
 
 class TeamFactory {
     
-    
     let teamFactory = Team()
     let heroesFactory = HeroesFactory()
     let team = Team()
     
     func getHeroeName() -> String{ // create the heroeName
         
-        //var choicePlayer: String
         var nameUnique: Bool = true
         var heroeName: String = ""
         var choicePlayer: String
         repeat {
             
-
             choicePlayer = readLine()!
-                
-            nameUnique = checkUniqueNameHeroe(heroeNameGiven : heroeName)
-                
+            nameUnique = checkUniqueNameHeroe(heroeNameGiven : choicePlayer)
+            
             
         }while nameUnique == false
         heroeName = choicePlayer
@@ -37,21 +33,21 @@ class TeamFactory {
     }
     
     
-    func composeTeam(heroeName : String, heroesInt: String) { // method createTeam
+    func composeTeam(heroeName : String, heroesInt: String, teamNameAlias: String) { // method createTeam
         
         
         let choicePlayer = heroesInt
-        let name = heroeName
         let heroe = heroesFactory.getHeroe(heroesInt: choicePlayer)!
-        heroe.heroeName = name
+        heroe.heroeName = heroeName
+        heroe.teamNameAlias = teamNameAlias
         teamFactory.arrayTeam.append(heroe)
         //print("We have: \(teamFactory.arrayTeam.count) Heroes in the array of teams")
         print(teamFactory.arrayTeam)
-       // return (name, choicePlayer)
+        print(heroe.teamNameAlias)
         
     }
     
-    func chooseYourWeapon(heroeName : String, heroesInt: String, weaponChoice: String) { // method for choosing the Weapon // First not choosable
+    func chooseYourWeapon(heroeName : String, heroesInt: String, weaponChoice: String) { // method for choosing the Weapon // First not possible
         
         let choicePlayer = heroesInt
         let name = heroeName
@@ -59,6 +55,7 @@ class TeamFactory {
         let choiceWeaponPlayer = weaponChoice
         switch choiceWeaponPlayer {
         case "1" :
+            
             let equipment = Equipment.Axe
             let equipmentEffect = equipment.rawValue // Printing the value of Axe
             print("The effect of your equipment is \(equipmentEffect)")
@@ -111,15 +108,22 @@ class TeamFactory {
         
         
     }
-    func statusTeam() {
+    func statusTeam(statusChoice: String) {
         
-        
+        let teamAlias = statusChoice
         print("that is the content of teamFactory.arrayTeam \(teamFactory.arrayTeam)")
         
         for element in 0..<teamFactory.arrayTeam.count {
-
-            print("We have in Team  \(teamFactory.arrayTeam[element].heroeName) :  A  \(teamFactory.arrayTeam[element].name) : with lifeStrength: \(teamFactory.arrayTeam[element].lifeStrength) and a shotStrength : \(teamFactory.arrayTeam[element].shotStrength) protected by \(teamFactory.arrayTeam[element].armorStrength) of armorStrength, is having \(teamFactory.arrayTeam[element].equipment) as an equipment")
             
+            if teamFactory.arrayTeam[element].teamNameAlias == teamAlias {
+                print("We have the player of type\(teamFactory.arrayTeam[element].heroeName) in Team  \(teamFactory.arrayTeam[element].teamNameAlias)")
+                
+            }
+            if teamFactory.arrayTeam[element].teamNameAlias == teamAlias {
+                print("We have the player of type\(teamFactory.arrayTeam[element].heroeName) in Team  \(teamFactory.arrayTeam[element].teamNameAlias)")
+                
+            }
+            //  print("We have in Team  \(teamFactory.arrayTeam[element].heroeName) :  A  \(teamFactory.arrayTeam[element].name) : with lifeStrength: \(teamFactory.arrayTeam[element].lifeStrength) and a shotStrength : \(teamFactory.arrayTeam[element].shotStrength) protected by \(teamFactory.arrayTeam[element].armorStrength) of armorStrength, is having \(teamFactory.arrayTeam[element].equipment) as an equipment")
         }
         
         //        print("We have: \(index) Heroes in the array of teams")
@@ -134,8 +138,10 @@ class TeamFactory {
             print("You must create two teams")
             game.gameMenu()
         }
-        else {
-            print("You would like to heal someone")
+        for element in 0..<teamFactory.arrayTeam.count {
+            if teamFactory.arrayTeam[element].type == "Wizard" {
+                print("You would like to heal someone")
+            }
         }
     }
     func fight() {
@@ -148,19 +154,41 @@ class TeamFactory {
         }
     }
     func checkUniqueNameHeroe(heroeNameGiven : String) -> Bool {
-        var unique: Bool = false
+        var unique: Bool = true
         let heroeNameEnter = heroeNameGiven
         
         for element in 0..<teamFactory.arrayTeam.count {
             if heroeNameEnter == teamFactory.arrayTeam[element].heroeName {
                 print("There is already an Heroe with that name")
                 print("Give another name to your Heroe")
-                unique = true
+                unique = false
             }
-
+            
+            
         }
         
         return unique
+    }
+    func checkNumberPlayerFirstTeam()-> Int {
+        var numberHeroesFirst: Int = 0 // number of heroes in First Team
+        
+        for element in 0..<teamFactory.arrayTeam.count { // counting number of heroes in First Team
+            if teamFactory.arrayTeam[element].teamNameAlias == "first" { // counting number of heroes in First Team
+                numberHeroesFirst += 1
+            }
+            
+        }
+        return numberHeroesFirst
+    }
+    func checkNumberPlayerSecondTeam()-> Int {
+        
+        var numberHeroesSecond: Int = 0// number of heroes in Second Team
+        for element in 0..<teamFactory.arrayTeam.count { // counting number of heroes in First Team
+            if teamFactory.arrayTeam[element].teamNameAlias == "second" { // counting number of heroes in Second Team
+                numberHeroesSecond += 1
+            }
+        }
+        return numberHeroesSecond
     }
 }
 
